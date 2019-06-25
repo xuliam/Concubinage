fbdc39abc25c23c99bb99ec59a618e485f7e9ffc
xuliam <lian.xu77@gmail.com>
2019-06-25 20:19:50 +1200

    First push

---
+++ src/AppMain.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class AppMain {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String[] nnNameArray = {"Mary", "Cindy", "Rose", "Linda", "Micky"};
+
+    }
+}
